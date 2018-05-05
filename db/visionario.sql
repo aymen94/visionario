96d2bbd589e236e9a88d5ba049503edfad395ce0
Andrea Mennillo <handymenny@outlook.com>
2018-05-05 14:21:13 +0200

    db: Fixup visionario.sql
    Make schema identifier uppercase,
    attribute identifiers lowercase and the first letter of
    table identifiers uppercase.
    Fix spelling of word "wishlist".
    Move primary keys and auto_increment into CREATE TABLE.
    Add missing foreign keys and primary keys.
    Change the type of price and total to DECIMAL(8,2).
    Change the type of password to BLOB, we will store JArgon2 encoded passwords.
    Adjust the types of some attributes to reduce their footprint.
    Use char instead of varchar where the content of attribute has a fixed size.
    Remove unneded comment, "-" and "`".
    Append schema to the name of tables.
    
    Change-Id: Ibae2c28586f786c3e3a73cf51a5223520c31e0f7
    Signed-off-by: Andrea Mennillo <handymenny@outlook.com>

--- db/visionario.sql
+++ db/visionario.sql
@@ -1,315 +1,154 @@
--- phpMyAdmin SQL Dump
--- version 4.8.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Creato il: Mag 04, 2018 alle 13:21
--- Versione del server: 10.1.31-MariaDB
--- Versione PHP: 7.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `visionario`
---
-
+-- Database: visionario
 -- --------------------------------------------------------
 
---
--- Struttura della tabella `address`
---
+CREATE SCHEMA VISIONARIO;
 
-CREATE TABLE `address` (
-  `ID` int(11) NOT NULL,
-  `User` int(11) NOT NULL,
-  `city` varchar(50) NOT NULL,
-  `country` varchar(50) NOT NULL,
-  `zip` varchar(5) NOT NULL,
-  `phone_number` varchar(12) NOT NULL,
-  `province` varchar(2) NOT NULL,
-  `consignee` varchar(50) NOT NULL,
-  `address_line` int(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Struttura della tabella Address
 
--- --------------------------------------------------------
+CREATE TABLE VISIONARIO.Address (
+  id INT UNSIGNED NOT NULL,
+  user INT UNSIGNED NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  zip CHAR(5) NOT NULL,
+  phone_number CHAR(12) NOT NULL,
+  province CHAR(2) NOT NULL,
+  consignee VARCHAR(50) NOT NULL,
+  address_line VARCHAR(70) NOT NULL,
+  FOREIGN KEY (user) REFERENCES `User`(id),
+  PRIMARY KEY (user,id)
+  );
 
---
--- Struttura della tabella `category`
---
+-- Struttura della tabella Category
 
-CREATE TABLE `category` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE VISIONARIO.Category (
+  id TINYINT UNSIGNED PRIMARY KEY,
+  `name` VARCHAR(20) NOT NULL
+);
 
--- --------------------------------------------------------
+-- Struttura della tabella Color
 
---
--- Struttura della tabella `color`
---
+CREATE TABLE VISIONARIO.Color (
+  id TINYINT UNSIGNED PRIMARY KEY,
+  hex CHAR(8) NOT NULL,
+  `name` VARCHAR(20) NOT NULL
+);
 
-CREATE TABLE `color` (
-  `ID` int(11) NOT NULL,
-  `hex` varchar(8) NOT NULL,
-  `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Struttura della tabella Coloration
 
--- --------------------------------------------------------
+CREATE TABLE VISIONARIO.Coloration (
+  product INT UNSIGNED NOT NULL,
+  color TINYINT UNSIGNED NOT NULL,
+  FOREIGN KEY (product) REFERENCES Product(id),
+  FOREIGN KEY (color) REFERENCES Color(id),
+  PRIMARY KEY (color, product)
+);
 
---
--- Struttura della tabella `coloration`
---
+-- Struttura della tabella Composition
 
-CREATE TABLE `coloration` (
-  `Product` int(11) NOT NULL,
-  `Color` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE VISIONARIO.Composition (
+  product INT UNSIGNED NOT NULL,
+  `order` BIGINT UNSIGNED NOT NULL,
+  quantity TINYINT UNSIGNED NOT NULL,
+  price DECIMAL(8,2) NOT NULL,
+  FOREIGN KEY (`order`) REFERENCES `Order`(id),
+  PRIMARY KEY (product, `order`)
+);
 
--- --------------------------------------------------------
+-- Struttura della tabella Image
 
---
--- Struttura della tabella `composition`
---
+CREATE TABLE VISIONARIO.Image (
+  id TINYINT UNSIGNED NOT NULL,
+  product INT UNSIGNED NOT NULL,
+  path VARCHAR(255) NOT NULL,
+  FOREIGN KEY (product) REFERENCES Product(id),
+  PRIMARY KEY (id,product)
+);
 
-CREATE TABLE `composition` (
-  `Product` int(11) NOT NULL,
-  `Order_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `price` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Struttura della tabella `Order`
 
--- --------------------------------------------------------
+CREATE TABLE VISIONARIO.`Order` (
+  id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  total DECIMAL(8,2) NOT NULL,
+  shipping_date DATE,
+  ordering_date DATE,
+  delivery_date DATE,
+  user INT UNSIGNED NOT NULL,
+  shipping_fees DECIMAL(8,2) NOT NULL,
+  `status` TINYINT UNSIGNED NOT NULL,
+  sign VARCHAR(70),
+  payment_method varchar(50),
+  FOREIGN KEY (user) REFERENCES User(id)
+);
 
---
--- Struttura della tabella `image`
---
+-- Struttura della tabella Product
 
-CREATE TABLE `image` (
-  `ID` int(11) NOT NULL,
-  `Product` int(11) NOT NULL,
-  `path` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE VISIONARIO.Product (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  size VARCHAR(4) NOT NULL,
+  discounted_price decimal(8,2),
+  sold MEDIUMINT UNSIGNED NOT NULL,
+  price DECIMAL(8,2) NOT NULL,
+  weight MEDIUMINT NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  `type` VARCHAR(20) NOT NULL,
+  description VARCHAR(2048),
+  available SMALLINT NOT NULL,
+  category TINYINT UNSIGNED NOT NULL,
+  FOREIGN KEY (category) REFERENCES Category(id)
+);
 
--- --------------------------------------------------------
+-- Struttura della tabella Review
 
---
--- Struttura della tabella `order_table`
---
+CREATE TABLE VISIONARIO.Review (
+  product INT UNSIGNED NOT NULL,
+  user INT UNSIGNED NOT NULL,
+  `date` DATE NOT NULL,
+  `comment` VARCHAR(100) NOT NULL,
+  score TINYINT UNSIGNED NOT NULL,
+  FOREIGN KEY (product) REFERENCES Product(id),
+  FOREIGN KEY (user) REFERENCES `User`(id),
+  PRIMARY KEY(product, user)
+);
 
-CREATE TABLE `order_table` (
-  `ID` int(11) NOT NULL,
-  `total` double NOT NULL,
-  `shipping_date` date NOT NULL,
-  `ordering_date` date NOT NULL,
-  `User` int(11) NOT NULL,
-  `delivery_date` date NOT NULL,
-  `shipping_fees` double NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `sign` varchar(70) NOT NULL,
-  `payment_method` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Struttura della tabella Showcase
 
--- --------------------------------------------------------
+CREATE TABLE VISIONARIO.Showcase (
+  product INT UNSIGNED NOT NULL,
+  top BIT(1) NOT NULL COMMENT "1->The product should go to carousel 0->Otherwise",
+  img VARCHAR(255) NOT NULL,
+  sale TINYINT UNSIGNED NOT NULL,
+  FOREIGN KEY (product) REFERENCES Product(id)
+);
 
---
--- Struttura della tabella `product`
---
+-- Struttura della tabella User
 
-CREATE TABLE `product` (
-  `ID` int(11) NOT NULL,
-  `size` varchar(4) NOT NULL,
-  `discount` decimal(8,0) NOT NULL,
-  `sold` int(11) NOT NULL,
-  `price` decimal(8,0) NOT NULL,
-  `weight` double NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `type` varchar(20) NOT NULL,
-  `description` text NOT NULL,
-  `available` tinyint(1) NOT NULL,
-  `Category` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE VISIONARIO.User (
+  id INT PRIMARY KEY,
+  `name` VARCHAR(20) NOT NULL,
+  surname VARCHAR(20),
+  email VARCHAR(50) NOT NULL,
+  `password` BLOB NOT NULL, -- JArgon2
+  sex BIT(1) COMMENT "1->Female 0->Male NULL-> otherwise",
+  date_of_birth date NOT NULL,
+  permission BIT(1) NOT NULL DEFAULT 0 COMMENT "1->Admin 0->otherwise",
+  UNIQUE(email)
+);
 
--- --------------------------------------------------------
+-- Struttura della tabella Variable
 
---
--- Struttura della tabella `review`
---
+CREATE TABLE VISIONARIO.Variable (
+  id VARCHAR(10) PRIMARY KEY,
+  `value` VARCHAR(50),
+  web_page VARCHAR(50) NOT NULL
+);
 
-CREATE TABLE `review` (
-  `Product` int(11) NOT NULL,
-  `User` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `comment` varchar(100) NOT NULL,
-  `score` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Struttura della tabella `Wishlist`
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella `showcase`
---
-
-CREATE TABLE `showcase` (
-  `Product` int(11) NOT NULL,
-  `top` tinyint(1) NOT NULL,
-  `img` varchar(255) NOT NULL,
-  `sale` int(11) NOT NULL,
-  `path` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `user`
---
-
-CREATE TABLE `user` (
-  `ID` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `surname` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `sex` varchar(1) NOT NULL,
-  `dateo_of_birth` date NOT NULL,
-  `permission` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `variable`
---
-
-CREATE TABLE `variable` (
-  `ID` int(11) NOT NULL,
-  `value` varchar(50) NOT NULL,
-  `web_page` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `whishlist`
---
-
-CREATE TABLE `whishlist` (
-  `Product` int(11) NOT NULL,
-  `User` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `address`
---
-ALTER TABLE `address`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `User` (`User`);
-
---
--- Indici per le tabelle `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indici per le tabelle `color`
---
-ALTER TABLE `color`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indici per le tabelle `coloration`
---
-ALTER TABLE `coloration`
-  ADD UNIQUE KEY `Product` (`Product`),
-  ADD UNIQUE KEY `Color` (`Color`);
-
---
--- Indici per le tabelle `composition`
---
-ALTER TABLE `composition`
-  ADD UNIQUE KEY `Product` (`Product`,`Order_id`);
-
---
--- Indici per le tabelle `image`
---
-ALTER TABLE `image`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Product` (`Product`);
-
---
--- Indici per le tabelle `order_table`
---
-ALTER TABLE `order_table`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `User` (`User`);
-
---
--- Indici per le tabelle `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Category` (`Category`);
-
---
--- Indici per le tabelle `review`
---
-ALTER TABLE `review`
-  ADD UNIQUE KEY `Product` (`Product`,`User`);
-
---
--- Indici per le tabelle `showcase`
---
-ALTER TABLE `showcase`
-  ADD UNIQUE KEY `Product` (`Product`);
-
---
--- Indici per le tabelle `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indici per le tabelle `variable`
---
-ALTER TABLE `variable`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indici per le tabelle `whishlist`
---
-ALTER TABLE `whishlist`
-  ADD UNIQUE KEY `Product` (`Product`,`User`);
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `product`
---
-ALTER TABLE `product`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `user`
---
-ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE VISIONARIO.Wishlist (
+  product INT UNSIGNED NOT NULL,
+  user INT UNSIGNED NOT NULL,
+  FOREIGN KEY (product) REFERENCES Product(id),
+  FOREIGN KEY (user) REFERENCES `User`(id),
+  PRIMARY KEY (user, product)
+);
