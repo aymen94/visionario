ba275aaeb344c43d164e81334049b94a8894cbc0
Andrea Mennillo <handymenny@outlook.com>
2018-05-07 21:42:18 +0200

    security: PasswordHash: Use SHA512 instead of SHA1
    Change-Id: I8a3d12444dbbec36475c41235002b8f28f6dfdfc
    Signed-off-by: Andrea Mennillo <handymenny@outlook.com>

--- src/security/PasswordHash.java
+++ src/security/PasswordHash.java
@@ -14,7 +14,7 @@ import java.security.spec.InvalidKeySpecException;
  */
 public class PasswordHash
 {
-    public static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
+    public static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA512";
 
     // The following constants may be changed without breaking existing hashes.
     public static final int SALT_BYTES =  128;
