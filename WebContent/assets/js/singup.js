9e94d2214ef3c1ac002f861a24977da9ec458a80
Aymen Naghmouchi <aymen.aymen@live.it>
2018-05-09 12:07:29 +0200

    fix dom

--- WebContent/assets/js/singup.js
+++ WebContent/assets/js/singup.js
@@ -71,7 +71,7 @@ pass.addEventListener('keyup',function () {
 passr.addEventListener('keyup',function () {
        if(passr.value==pass.value) {
             document.getElementById('passM').classList.remove('fa-times');
-            document.getElementBayId('passM').classList.add('fa-check');
+            document.getElementById('passM').classList.add('fa-check');
             document.getElementById('passM').style.color = "green";
        }else {
            document.getElementById('passM').classList.remove('fa-check');
