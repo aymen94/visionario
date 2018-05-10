8e851ebd1967ffac5aa17437b31b3912f3470547
Andrea Mennillo <handymenny@outlook.com>
2018-05-10 12:00:38 +0200

    Add cartItem
    Change-Id: Ic5c55045dbb4fb4c4bae1efca595dbbb55449142

---
+++ src/model/CartBean.java
@@ -0,0 +1,40 @@
+package model;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CartBean {
+
+    public CartBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+        private Map<CartItem,Integer> items = new HashMap<CartItem,Integer>();
+
+        public Set<CartItem> getItems() {
+            return items.keySet();
+        }
+
+        public int getOrder(CartItem item) {
+            return items.get(item);
+        }
+
+        public void put(CartItem item, int quantity) {
+            items.put(item, quantity);
+        }
+        
+        public void empty() {
+            this.items = new HashMap<CartItem,Integer>();
+        }
+
+        public void remove(CartItem item) {
+            items.remove(item);
+        }
+        
+        public void getSize() {
+            items.size();
+        }
+        
+        
+
+}
---
+++ src/model/CartItem.java
@@ -0,0 +1,14 @@
+package model;
+
+public class CartItem extends Product{
+    VariantBean variant;
+    
+    public CartItem() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public VariantBean getVariant() {
+        
+        return variant;
+    }
+}
