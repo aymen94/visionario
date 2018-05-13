dad97887940ff58dcdada4615963c27df70b0b04
Andrea Mennillo <handymenny@outlook.com>
2018-05-13 15:22:37 +0200

    model: ProductModel: Imporove sql query and add doRetrieveAll()
    Change-Id: I4c0934f4bbb93f30350d1e12f10dfae52c1ed437

--- src/model/ProductModel.java
+++ src/model/ProductModel.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import config.Ds;
 import model.bean.ProductBean;
@@ -48,8 +49,8 @@ public class ProductModel {
 
         ProductBean bean = new ProductBean();
 
-        String selectSQL = "SELECT Product.id, title, priceMin, priceMax, numReviews, path FROM Product, Image WHERE Product.id = ? and Image.product=Product.id AND Image.path LIKE '%default%'";
-
+        String selectSQL = "SELECT Product.id, title, priceMin, priceMax, numReviews, path FROM Product, Image"
+                + " WHERE Product.id=? AND Image.product=Product.id AND Image.path LIKE '%default%' AND Image.VARIANT=1";
         try {
             connection = Ds.getConnection();
             preparedStatement = connection.prepareStatement(selectSQL);
@@ -78,6 +79,43 @@ public class ProductModel {
         return bean;
     }
 
+    public synchronized ArrayList<ProductBean> doRetrieveAll() throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        ArrayList<ProductBean> beans = new ArrayList<>();
+        ProductBean bean;
+
+        String selectSQL = "SELECT  Product.id,title, priceMin, priceMax, numReviews, path FROM Product, Image "
+                + "WHERE Image.product=Product.id AND path LIKE '%default%' AND VARIANT=1";
+        try {
+            connection = Ds.getConnection();
+            preparedStatement = connection.prepareStatement(selectSQL);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                bean= new ProductBean();
+                bean.setId(rs.getLong("id"));
+                bean.setTitle(rs.getString("title"));
+                bean.setLowerPrice(rs.getBigDecimal("priceMin"));
+                bean.setMaxPrice(rs.getBigDecimal("priceMax"));
+                bean.setDefaultImage(rs.getString("path"));
+                bean.setNumReviews(rs.getInt("numReviews"));
+                beans.add(bean);
+            }
+
+        } finally {
+            try {
+                if (preparedStatement != null)
+                    preparedStatement.close();
+            } finally {
+                if (connection != null)
+                    connection.close();
+            }
+        }
+        return beans;
+    }
 
  /*
     @Override
