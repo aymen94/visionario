fe416b74b2179cd4be470fd710b0f31b3206fb50
Aymen Naghmouchi <aymen.aymen@live.it>
2018-05-17 11:01:37 +0200

    fix query
    Signed-off-by: Aymen Naghmouchi <aymen.aymen@live.it>

--- src/model/Query.java
+++ src/model/Query.java
@@ -21,5 +21,7 @@ public class Query {
     protected static final String productAll = "SELECT  Product.id,title, priceMin, priceMax, numReviews, path FROM Product, Image "+
                                                "WHERE Image.product=Product.id AND path LIKE '%default%' AND VARIANT=1";
 
-    protected static final String productDetail = "SELECT title,description,category,gender,numReviews FROM visionario.product WHERE id=?";
+    protected static final String productDetail = "SELECT title,description,category,gender,numReviews,priceMin, priceMax, path " +
+                                                  "FROM visionario.product, visionario.image " +
+                                                  "WHERE id=? AND Image.product=Product.id AND Image.path LIKE '%default%' AND Image.VARIANT=1";
 }
