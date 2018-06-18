d4dbf9fa994bd7a6f1b8ada34faee5261bfbc52f
Aymen Naghmouchi <aymen.aymen@live.it>
2018-06-18 13:05:14 +0200

    change setting of algorithm

--- src/security/PasswordHash.java
+++ src/security/PasswordHash.java
@@ -18,7 +18,7 @@ public class PasswordHash
 
     // The following constants may be changed without breaking existing hashes.
     public static final int SALT_BYTES =  1;
-    public static final int HASH_BYTES = 1;
+    public static final int HASH_BYTES = 128;
     public static final int PBKDF2_ITERATIONS = 1;
 
     public static final int ITERATION_INDEX = 0;
@@ -167,38 +167,11 @@ public class PasswordHash
      */
     public static void main(String[] args)
     {
+
         try
         {
-            // Print out 10 hashes
-            for(int i = 0; i < 10; i++)
-                System.out.println(PasswordHash.createHash("p\r\nassw0Rd!"));
+            System.out.println(PasswordHash.createHash("aygusadjfksmgjndsb").length());
 
-            // Test password validation
-            boolean failure = false;
-            System.out.println("Running tests...");
-            for(int i = 0; i < 100; i++)
-            {
-                String password = ""+i;
-                String hash = createHash(password);
-                String secondHash = createHash(password);
-                if(hash.equals(secondHash)) {
-                    System.out.println("FAILURE: TWO HASHES ARE EQUAL!");
-                    failure = true;
-                }
-                String wrongPassword = ""+(i+1);
-                if(validatePassword(wrongPassword, hash)) {
-                    System.out.println("FAILURE: WRONG PASSWORD ACCEPTED!");
-                    failure = true;
-                }
-                if(!validatePassword(password, hash)) {
-                    System.out.println("FAILURE: GOOD PASSWORD NOT ACCEPTED!");
-                    failure = true;
-                }
-            }
-            if(failure)
-                System.out.println("TESTS FAILED!");
-            else
-                System.out.println("TESTS PASSED!");
         }
         catch(Exception ex)
         {
