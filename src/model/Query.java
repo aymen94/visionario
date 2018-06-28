15a106554bba9ad8cdad5ffe99ee05c1334b23c1
Aymen Naghmouchi <aymenadvance@gmail.com>
2018-06-28 13:31:27 +0200

    add new query login and registration and add color
    Signed-off-by: Aymen Naghmouchi <aymenadvance@gmail.com>

--- src/model/Query.java
+++ src/model/Query.java
@@ -4,13 +4,11 @@ public class Query {
 
     protected static final String category = "SELECT id, name FROM Category WHERE id=?";
 
-    protected static final String categoryAll = "SELECT id, name FROM Category";
-
     protected static final String variantByProd = "SELECT id,size,discounted_price,sold,price,weight,available,color,path "+
                                                   "FROM ProductVariant p, Image i " +
                                                   "WHERE p.product=?  and i.product=p.product and i.variant = p.id";
 
-    protected static final String variantByKey = "SELECT size,discounted_price,sold,price,weight,available,path " +
+    protected static final String variantByKey = "SELECT size,discounted_price,sold,price,weight,available,color,path " +
                                                  "FROM ProductVariant p, Image i " +
                                                  "WHERE p.product=? and p.id=? and i.product=p.product and i.variant = p.id";
 
@@ -21,7 +19,7 @@ public class Query {
     protected static final String productAll = "SELECT  Product.id,title, priceMin, priceMax, numReviews, path FROM Product, Image "+
                                                "WHERE Image.product=Product.id AND path LIKE '%default%' AND VARIANT=1";
 
-    protected static final String productDetail = "SELECT title,description,category,gender,numReviews,priceMin, priceMax, path " +
+    protected static final String productDetail = "SELECT title,description,category,gender,numReviews,priceMin,priceMax,path " +
                                                   "FROM Product, Image " +
                                                   "WHERE id=? AND Image.product=Product.id AND Image.path LIKE '%default%' AND Image.variant=1";
 
