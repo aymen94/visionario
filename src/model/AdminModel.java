ffe9db05faed0293af6f235c2223edfcbfa06350
Aymen Naghmouchi <aymenadvance@gmail.com>
2018-07-06 18:15:36 +0200

    add adminmodel for admin dashboard
    Signed-off-by: Aymen Naghmouchi <aymenadvance@gmail.com>

---
+++ src/model/AdminModel.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018. Visionario
+ */
+
+package model;
+
+import config.Ds;
+import java.math.BigDecimal;
+import java.sql.*;
+
+
+public class AdminModel {
+
+    public int doRetrieveByNumber(String name) throws SQLException {
+        Connection conn = Ds.getConnection();
+        Statement st = null;
+        ResultSet rs;
+        int num=0;
+        try {
+            st = conn.createStatement();
+            rs= st.executeQuery(Query.numberInfo+name);
+            if (rs.next())
+               num=rs.getInt(1);
+        } finally {
+            st.close();
+            conn.close();
+        }
+        return num;
+    }
+
+    public BigDecimal doRetrieveRevenue() throws SQLException {
+        Connection conn = Ds.getConnection();
+        Statement st = null;
+        ResultSet rs;
+        BigDecimal num = null;
+        try {
+            st = conn.createStatement();
+            rs= st.executeQuery(Query.revenue);
+            if (rs.next())
+                num=rs.getBigDecimal(1);
+        } finally {
+            st.close();
+            conn.close();
+        }
+        return num;
+    }
+
+}
