cc7f058c21dadd17d09eb5c6e89f8d55316d88d3
Aymen Naghmouchi <aymenadvance@gmail.com>
2018-07-16 16:38:39 +0200

    small fix

--- src/controller/Pay.java
+++ src/controller/Pay.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,65 +19,52 @@ import model.bean.OrderBean;
 /**
  * Servlet implementation class Pay
  */
-@WebServlet("/Pay")
+@WebServlet("/payment")
 public class Pay extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException {
 		// TODO Auto-generated method stub
-	       try {
-	    HttpSession session=request.getSession();
-	    short address =Short.parseShort(request.getParameter("addressRadio"));
-	    long user=(long) session.getAttribute("userId");
-	    String paymentMethod= request.getParameter("pay");
-	    AddressBean addr=new AddressModel().doRetrieveByUser_id(user, address);
-	    OrderBean bean= new OrderBean();
-	    BigDecimal total=new BigDecimal(request.getParameter("total"));
-	    bean.setUser(user);
-	    bean.setShippingFees(BigDecimal.valueOf(0,2));
-	    bean.setAddress(addr.toString());
-	    bean.setOrderingDate(java.sql.Date.valueOf(LocalDate.now()));
-	    bean.setTotal(total);
-	    bean.setConsignee(addr.getConsignee());
-	    bean.setPaymentMethod(paymentMethod);
-	    bean.setStatus((short) 0);
-	    if(paymentMethod.toLowerCase().contains("card"))
-	       bean.setStatus((short) 1);
+		HttpSession session=request.getSession();
+		try {
+			short address =Short.parseShort(request.getParameter("addressRadio"));
+			long user=(long) session.getAttribute("userId");
+			String paymentMethod= request.getParameter("pay");
+			AddressBean addr=new AddressModel().doRetrieveByUser_id(user, address);
+			OrderBean bean= new OrderBean();
+			BigDecimal total=new BigDecimal(request.getParameter("total"));
+			bean.setUser(user);
+			bean.setShippingFees(BigDecimal.valueOf(0,2));
+			bean.setAddress(addr.toString());
+			bean.setOrderingDate(java.sql.Date.valueOf(LocalDate.now()));
+			bean.setTotal(total);
+			bean.setConsignee(addr.getConsignee());
+			bean.setPaymentMethod(paymentMethod);
+			bean.setStatus((short) 0);
+			if(paymentMethod.toLowerCase().contains("card"))
+			   bean.setStatus((short) 1);
 
-            if(new OrderModel().doSave(bean,(CartBean) session.getAttribute("checkout")))
-            {
-                session.setAttribute("checkout",null);
-                if(paymentMethod.toLowerCase().contains("card"))
-                {
-                request.getRequestDispatcher("/card.html").forward(request, response);
-                }
-                else
-                {
-                request.getRequestDispatcher("/bonifico.html").forward(request, response);
-                }
-            }
-            else
-            {
-                throw new Exception();
-            }
+				if(new OrderModel().doSave(bean,(CartBean) session.getAttribute("checkout")))	{
+					session.setAttribute("checkout",null);
+					session.setAttribute("cart",null);
+					request.getRequestDispatcher("/pay").forward(request, response);
+				}else{
+						session.setAttribute("checkout",null);
+					    session.setAttribute("cart",null);
+					    request.getRequestDispatcher("/cart").forward(request,response);
+					}
         } catch (Exception e) {
+	       	session.setAttribute("checkout",null);
             e.printStackTrace();
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            return;
         }
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
 		doGet(request, response);
 	}
 
