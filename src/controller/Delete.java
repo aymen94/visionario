d72f66135e9316aeae567613ec2d3ac7770a1e31
Aymen Naghmouchi <aymenadvance@gmail.com>
2018-07-25 00:27:06 +0200

    add delete, and final release XD

---
+++ src/controller/Delete.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018. Visionario
+ */
+
+package controller;
+
+import model.UserModel;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/delete")
+public class Delete extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        doPost(req, resp);    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        String type;
+        long id;
+        try {
+            if (!(new CheckUser(req.getSession()).isPermission()))
+                return;
+        }
+        catch (Exception ex) {
+          return;
+        }
+        type = req.getParameter("type");
+        id = Long.parseLong(req.getParameter("id"));
+        try {
+            switch (type) {
+
+                case   "usr": new UserModel().doDelete(id);
+                              break;
+
+                default:      break;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
